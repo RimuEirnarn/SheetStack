568b75f574fa6e26eba792d39a0e801fc75f08a8
Rimu Eirnarn <Rimuru720@proton.me>
2025-08-24 11:15:31 +0700

    added some info

--- props/cmps/help.py
+++ props/cmps/help.py
@@ -3,6 +3,7 @@
 
 import curses
 
+from ..config import APP_CONFIG
 from ..data import ReturnType
 from ..component import Component
 
@@ -13,6 +14,14 @@ class Help(Component):
     def __init__(self) -> None:
         super().__init__()
         self._key_events = {curses.KEY_LEFT: lambda: ReturnType.BACK}
+        self._keystrokes = [
+            ("[LEFT]", "Go back"),
+            ("[RIGHT]", "Choose selected entry"),
+            ("[ENTER]", "Choose selected entry"),
+            ("[UP]", "Move selection up"),
+            ("[DOWN]", "Move selection down"),
+            ("[Q]", "Quit")
+        ]
 
     def draw(self, stdscr: curses.window):
         stdscr.addstr(
@@ -21,3 +30,12 @@ class Help(Component):
             "You can install PaperMC, select the default version, or run the server.",
         )
         stdscr.addstr(1, 0, "Use [LEFT] to go back.")
+
+        stdscr.addstr(4, 0, "Additionally, you can check your configuration, it's here:")
+        stdscr.addstr(5, 0, f"{APP_CONFIG}")
+        stdscr.addstr(7, 0, "Wanna shortcut?")
+        stdscr.addstr(8, 0, f"cd {APP_CONFIG}")
+
+        stdscr.addstr(10, 0, "Keyboard shortcuts: ")
+        for index, (key, label) in enumerate(self._keystrokes):
+            stdscr.addstr(11 + index, 0, f"{key} -> {label}")
